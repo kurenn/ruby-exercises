23db0ce7c0f792c4a2befa414502475c9e204fcd
Abraham Kuri <kurenn@icalialabs.com>
2021-04-20 18:26:30 -0500

    Adds ints and floats exercises

---
+++ Module1/data-types/ints_and_floats_exercise.rb
@@ -0,0 +1,46 @@
+# Ruby Dojo
+# Docs:
+# https://ruby-doc.org/core-3.0.1/
+#
+
+# Perform basic operations with Ruby
+# Highlights:
+ # - You can perform operations between integers and floats
+ # - Keep track of your variables
+ 
+# How to run this file
+# 1. Open a terminal and `cd` to where the project is
+  # cd path/to/ruby-exercises/Module1/data-types
+# 2. Run the file
+  # ruby ints_and_floats_exercise.rb
+
+# Exercise 1:
+# Add the necessary code to add the necessary numbers to sum 342
+
+# Exercise 2:
+# Add the necessary code to substract the necessary numbers to equal -43
+
+# Exercise 3:
+# Add the necessary code to multiply the the necessary numbers to get 144
+
+# Exercise 4:
+# Add the necessary code to divide the the necessary numbers to get 5
+
+# Exercise 5:
+# Calculate the area and perimeter for a square
+
+# Exercise 6:
+# Calculate the area and perimeter for a rectangle
+
+# Exercise 7:
+# Calculate the area and perimeter for a triangle
+
+# Exercise 8:
+# Calculate the area and perimeter for a circle
+
+# Exercise 9:
+# Get the average age from your family
+
+# Exercise 10:
+# Calculate the proportion for each fruit found on a bowl:
+ # 3 bananas, 2 apples, 1 watermelon, 5 kiwis, 10 strawberries
