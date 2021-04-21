c25f7c522529e62d9c708b35003339fd8ef01ae7
Abraham Kuri <kurenn@icalialabs.com>
2021-04-20 19:05:26 -0500

    Adds the strings exercise

---
+++ Module1/data-types/strings_exercise.rb
@@ -0,0 +1,95 @@
+# Ruby Dojo
+# Docs:
+# https://ruby-doc.org/core-3.0.1/
+#
+
+# Working with Strings in Ruby
+# Highlights:
+ # - You can join multiple strings using interpolation or concatenation
+ # - You use other basic operator other than "+" with strings
+  
+# How to run this file
+# 1. Open a terminal and `cd` to where the project is
+  # cd path/to/ruby-exercises/Module1/data-types
+# 2. Run the file
+  # ruby strings_exercise.rb
+
+# You can use the method "puts" to output the result to the console
+# puts a_variable
+# puts "Hello World!"
+
+# Exercise 1:
+# Use a method to transform the string so that every letter on the string is uppercase
+alice = "alice"
+alice.change_this_method
+#=> ALICE
+
+# Exercise 2:
+# Use a method to transform the string so that only the first letter is uppercase
+alice = "aLiCe"
+alice.change_this_method
+#=> Alice
+
+# Exercise 3:
+# Use a method to transform the string so that it reads backwards
+phrase = "The most inflated egos are often the most fragile."
+phrase.change_this_method
+#=> ".eligarf tsom eht netfo era soge detalfni tsom ehT"
+
+# Exercise 4:
+# Use a method to transform the string so that every "o" turns to "u"
+phrase = "The most inflated egos are often the most fragile."
+phrase.change_this_method
+#=> "The must inflated egus are uften the must fragile."
+
+# Exercise 5:
+# Use a method to remove the duplicated ! sign 
+phrase = "Hello World!!"
+phrase.change_this_method
+#=> "Hello World!"
+
+# Exercise 6:
+# Use a method to remove the vocals from the string
+phrase = "The most inflated egos are often the most fragile."
+phrase.change_this_method
+#=> "Th mst nfltd gs r ftn th mst frgl."
+
+# Exercise 7:
+# Use a method to count all the characters from the string
+phrase = "The most inflated egos are often the most fragile."
+phrase.change_this_method
+#=> 50
+
+# Exercise 8:
+# Use a method to invert the string case
+phrase = "The most Inflated egOs are ofTen The mOsT Fragile."
+phrase.change_this_method
+#=> "tHE MOST iNFLATED EGoS ARE OFtEN tHE MoSt fRAGILE."
+
+# Exercise 9:
+# Use a method check if the word 'fragile' exists on the phrase
+phrase = "The most inflated egos are often the most fragile."
+phrase.change_this_method
+#=> true
+
+# Exercise 10:
+# Concatenate the two provided strings
+greetings = "Hello, my name is:"
+name = "Alice"
+#=> "Hello, my name is: Alice!"
+
+# Exercise 11:
+# Interpolate the two provided strings
+greetings = "Hello, my name is:"
+name = "Alice"
+#=> "Hello, my name is: Alice!"
+
+# Exercise 12:
+# Repeat the full message 200 times. Mind the spaces
+greetings = "Hello, my name is:"
+name = "Alice"
+#=> "Hello, my name is: Alice! Hello, my name is: Alice! Hello, my name is: Alice!..."
+
+
+
+
